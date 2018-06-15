942e3304ee6bcacb9bfd1f66ee814673b44da3c4
Sergey Andreev <Sergey.Andreev@jetbrains.com>
2018-06-15 15:58:35 +0300

    initial project

---
+++ Tests_xUnit/NodesCoverage.cs
@@ -0,0 +1,15 @@
+﻿using TriangleSample;
+using Xunit;
+
+namespace TestProject1
+{
+    public class NodesCoverage
+    {
+        [Fact]
+        public void test1() {
+            int[] testArr = {3, 4, 5};
+            Triangle testTr = new Triangle();
+            Assert.Equal("Разносторонний", testTr.TypeOfTriangle(testArr));
+        }
+    }
+}
---
+++ TriangleSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TriangleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
